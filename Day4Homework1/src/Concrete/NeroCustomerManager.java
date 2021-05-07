daa0800564e2e2bbda41c36b02cd05cd55bff6d3
Meryem Sude ÇETİN <56775168+Sude4@users.noreply.github.com>
2021-05-07 18:48:38 +0300

    Delete Day4Homework1 directory

--- Day4Homework1/src/Concrete/NeroCustomerManager.java
+++
@@ -1,9 +0,0 @@
-package Concrete;
-import Abstract.BaseCustomerManager;
-import Abstract.CustomerCheckService;
-import Entities.Customer;
-@SuppressWarnings("unused")
-public class NeroCustomerManager extends BaseCustomerManager{
-		 
-	 }
-	 
--- Day4Homework1/src/tr/gov/nvi/tckimlik/WS/Abstract.java
+++
@@ -1,5 +0,0 @@
-//package tr.gov.nvi.tckimlik.WS;
-
-//public class Abstract {
-
-//}
